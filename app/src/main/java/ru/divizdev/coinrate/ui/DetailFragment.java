f1dcd8a914e0341aa8898d1a1b820959bd7824c9
diviz <diviz.dev@gmail.com>
2018-03-15 18:13:18 +0300

    step 2 of a new detailed view

--- app/src/main/java/ru/divizdev/coinrate/ui/DetailFragment.java
+++ app/src/main/java/ru/divizdev/coinrate/ui/DetailFragment.java
@@ -25,6 +25,7 @@ public class DetailFragment extends Fragment  {
     private TextView _detailMarketCap; //detail_max_supply
     private TextView _detail24hVolume; //detail_total_supply
     private ImageView _logo;
+    private TextView _detailSymbolCoin;
 
 
 
@@ -69,8 +70,10 @@ public class DetailFragment extends Fragment  {
         _detailMarketCap = view.findViewById(R.id.detail_market_cap);
         _detail24hVolume = view.findViewById(R.id.detail_24h_volume);
         _logo = view.findViewById(R.id.detail_image_view);
+        _detailSymbolCoin = view.findViewById(R.id.detail_symbol_coin);
 
         _detailNameCoin.setText(_coinRateUI.getName());
+        _detailSymbolCoin.setText(_coinRateUI.getSymbol());
         _detail24hVolume.setText(String.format("%s %s",  _coinRateUI.getUIVolume24(), _coinRateUI.getUICurrency()));
         _detailMarketCap.setText(String.format("%s %s", _coinRateUI.getUIMarketCap(), _coinRateUI.getUICurrency()));
         _detailAvailableSupply.setText(String.format("%s %s", _coinRateUI.getUIAvailableSupply(), _coinRateUI.getSymbol()));
