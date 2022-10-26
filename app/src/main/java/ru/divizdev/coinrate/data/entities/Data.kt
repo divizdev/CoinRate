99b96c8deb6e7ba03912d70e008cfd505bdb2a29
divizdev <diviz.dev@gmail.com>
2022-10-27 00:02:02 +0300

    Migrate to kotlin (#16)
    * refactoring the entity model

---
+++ app/src/main/java/ru/divizdev/coinrate/data/entities/Data.kt
@@ -0,0 +1,59 @@
+package ru.divizdev.coinrate.data.entities
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import java.math.BigDecimal
+
+data class Data(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+
+    @SerializedName("name")
+    @Expose
+    var name: String? = null,
+
+    @SerializedName("symbol")
+    @Expose
+    var symbol: String? = null,
+
+    @SerializedName("slug")
+    @Expose
+    var slug: String? = null,
+
+    @SerializedName("cmc_rank")
+    @Expose
+    var cmcRank: Int? = null,
+
+    @SerializedName("num_market_pairs")
+    @Expose
+    var numMarketPairs: BigDecimal? = null,
+
+    @SerializedName("circulating_supply")
+    @Expose
+    var circulatingSupply: BigDecimal? = null,
+
+    @SerializedName("total_supply")
+    @Expose
+    var totalSupply: BigDecimal? = null,
+
+    @SerializedName("max_supply")
+    @Expose
+    var maxSupply: BigDecimal? = null,
+
+    @SerializedName("last_updated")
+    @Expose
+    var lastUpdated: String? = null,
+
+    @SerializedName("date_added")
+    @Expose
+    var dateAdded: String? = null,
+
+    @SerializedName("tags")
+    @Expose
+    var tags: List<String>? = null,
+
+    @SerializedName("quote")
+    @Expose
+    var quote: Map<String, Price> = mutableMapOf(),
+)
--- app/src/main/java/ru/divizdev/coinrate/presentation/entities/CoinRateUI.kt
+++ app/src/main/java/ru/divizdev/coinrate/presentation/entities/CoinRateUI.kt
@@ -1,7 +1,7 @@
 package ru.divizdev.coinrate.presentation.entities
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import ru.divizdev.coinrate.BuildConfig
 import ru.divizdev.coinrate.R
 import ru.divizdev.coinrate.data.entities.ApiData
@@ -11,101 +11,25 @@ import java.math.BigDecimal
 import java.math.MathContext
 import java.util.*
 
-class CoinRateUI : Parcelable {
-    val currency: String?
-    val id: String?
-    val name: String?
-    val symbol: String?
-    val rank: Int
-    val price: BigDecimal?
-    val marketCap: BigDecimal?
-    val percentChange1h: Double
-    val percentChange24h: Double
-    val percentChange7d: Double
-    val availableSupply: BigDecimal?
-    val totalSupply: BigDecimal?
-    val maxSupply: BigDecimal?
-    val lastUpdated: Int
-    val volume24h: BigDecimal?
+@Parcelize
+data class CoinRateUI(
+    val currency: String?,
+    val id: String?,
+    val name: String?,
+    val symbol: String?,
+    val rank: Int,
+    val price: BigDecimal?,
+    val marketCap: BigDecimal?,
+    val percentChange1h: Double,
+    val percentChange24h: Double,
+    val percentChange7d: Double,
+    val availableSupply: BigDecimal?,
+    val totalSupply: BigDecimal?,
+    val maxSupply: BigDecimal?,
+    val lastUpdated: Int,
+    val volume24h: BigDecimal?,
+) : Parcelable {
 
-    constructor(
-        currency: String?,
-        id: String?,
-        name: String?,
-        symbol: String?,
-        rank: Int,
-        price: BigDecimal?,
-        marketCap: BigDecimal?,
-        percentChange1h: Double,
-        percentChange24h: Double,
-        percentChange7d: Double,
-        availableSupply: BigDecimal?,
-        totalSupply: BigDecimal?,
-        maxSupply: BigDecimal?,
-        lastUpdated: Int,
-        volume24h: BigDecimal?
-    ) {
-        this.currency = currency
-        this.id = id
-        this.name = name
-        this.symbol = symbol
-        this.rank = rank
-        this.price = price
-        this.marketCap = marketCap
-        this.percentChange1h = percentChange1h
-        this.percentChange24h = percentChange24h
-        this.percentChange7d = percentChange7d
-        this.availableSupply = availableSupply
-        this.totalSupply = totalSupply
-        this.maxSupply = maxSupply
-        this.lastUpdated = lastUpdated
-        this.volume24h = volume24h
-    }
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as CoinRateUI
-        if (rank != that.rank) return false
-        if (java.lang.Double.compare(that.percentChange1h, percentChange1h) != 0) return false
-        if (java.lang.Double.compare(that.percentChange24h, percentChange24h) != 0) return false
-        if (java.lang.Double.compare(that.percentChange7d, percentChange7d) != 0) return false
-        if (lastUpdated != that.lastUpdated) return false
-        if (if (currency != null) currency != that.currency else that.currency != null) return false
-        if (if (id != null) id != that.id else that.id != null) return false
-        if (if (name != null) name != that.name else that.name != null) return false
-        if (if (symbol != null) symbol != that.symbol else that.symbol != null) return false
-        if (if (price != null) price != that.price else that.price != null) return false
-        if (if (marketCap != null) marketCap != that.marketCap else that.marketCap != null) return false
-        if (if (availableSupply != null) availableSupply != that.availableSupply else that.availableSupply != null) return false
-        if (if (totalSupply != null) totalSupply != that.totalSupply else that.totalSupply != null) return false
-        if (if (maxSupply != null) maxSupply != that.maxSupply else that.maxSupply != null) return false
-        return if (volume24h != null) volume24h == that.volume24h else that.volume24h == null
-    }
-
-    override fun hashCode(): Int {
-        var result: Int
-        var temp: Long
-        result = currency?.hashCode() ?: 0
-        result = 31 * result + (id?.hashCode() ?: 0)
-        result = 31 * result + (name?.hashCode() ?: 0)
-        result = 31 * result + (symbol?.hashCode() ?: 0)
-        result = 31 * result + rank
-        result = 31 * result + (price?.hashCode() ?: 0)
-        result = 31 * result + (marketCap?.hashCode() ?: 0)
-        temp = java.lang.Double.doubleToLongBits(percentChange1h)
-        result = 31 * result + (temp xor (temp ushr 32)).toInt()
-        temp = java.lang.Double.doubleToLongBits(percentChange24h)
-        result = 31 * result + (temp xor (temp ushr 32)).toInt()
-        temp = java.lang.Double.doubleToLongBits(percentChange7d)
-        result = 31 * result + (temp xor (temp ushr 32)).toInt()
-        result = 31 * result + (availableSupply?.hashCode() ?: 0)
-        result = 31 * result + (totalSupply?.hashCode() ?: 0)
-        result = 31 * result + (maxSupply?.hashCode() ?: 0)
-        result = 31 * result + lastUpdated
-        result = 31 * result + (volume24h?.hashCode() ?: 0)
-        return result
-    }
 
     val uiVolume24: String
         get() = LocaleUtils.formatBigDecimal(volume24h)
@@ -163,50 +87,9 @@ class CoinRateUI : Parcelable {
         return 0
     }
 
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(currency)
-        dest.writeString(id)
-        dest.writeString(name)
-        dest.writeString(symbol)
-        dest.writeInt(rank)
-        dest.writeSerializable(price)
-        dest.writeSerializable(marketCap)
-        dest.writeDouble(percentChange1h)
-        dest.writeDouble(percentChange24h)
-        dest.writeDouble(percentChange7d)
-        dest.writeSerializable(availableSupply)
-        dest.writeSerializable(totalSupply)
-        dest.writeSerializable(maxSupply)
-        dest.writeInt(lastUpdated)
-        dest.writeSerializable(volume24h)
-    }
 
-    protected constructor(`in`: Parcel) {
-        currency = `in`.readString()
-        id = `in`.readString()
-        name = `in`.readString()
-        symbol = `in`.readString()
-        rank = `in`.readInt()
-        price = `in`.readSerializable() as BigDecimal?
-        marketCap = `in`.readSerializable() as BigDecimal?
-        percentChange1h = `in`.readDouble()
-        percentChange24h = `in`.readDouble()
-        percentChange7d = `in`.readDouble()
-        availableSupply = `in`.readSerializable() as BigDecimal?
-        totalSupply = `in`.readSerializable() as BigDecimal?
-        maxSupply = `in`.readSerializable() as BigDecimal?
-        lastUpdated = `in`.readInt()
-        volume24h = `in`.readSerializable() as BigDecimal?
-    }
 
-    companion object CREATOR : Parcelable.Creator<CoinRateUI> {
-        override fun createFromParcel(parcel: Parcel): CoinRateUI {
-            return CoinRateUI(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CoinRateUI?> {
-            return arrayOfNulls(size)
-        }
+    companion object {
         @JvmStatic
         fun convertList(apiData: ApiData, currency: String): List<CoinRateUI> {
             val result: MutableList<CoinRateUI> = ArrayList()
@@ -234,7 +117,10 @@ class CoinRateUI : Parcelable {
                         price.percentChange24h?.round(MathContext(2))?.toDouble() ?: 0.toDouble(),
                         price.percentChange7d?.round(MathContext(2))?.toDouble() ?: 0.toDouble(),
                         datum.circulatingSupply,
-                        datum.totalSupply, datum.maxSupply, 0, price.volume24h
+                        datum.totalSupply,
+                        datum.maxSupply,
+                        0,
+                        price.volume24h
                     )
                 )
             }
